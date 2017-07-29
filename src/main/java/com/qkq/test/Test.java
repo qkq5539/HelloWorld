97bb947c436f7454971d80ce38b43b9ae691ad0a
qiaokaiqiang <914052561@qq.com>
2017-07-30 01:26:19 +0800

    修改Test类

--- src/main/java/com/qkq/test/Test.java
+++ src/main/java/com/qkq/test/Test.java
@@ -4,7 +4,11 @@ package com.qkq.test;
  * Created by Administrator on 2017/7/30.
  */
 public class Test {
-    public static void main(String... args){
-        System.out.print("hello World");
+    public static void main(String... args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setName("张三");
+        helloWorld.setAddress("南窑头");
+        System.out.print(helloWorld.getAddress());
+        System.out.print(helloWorld.getName());
     }
 }
