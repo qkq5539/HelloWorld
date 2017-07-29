be5f070a5275ca3775d042790f4a054d98af0c10
qiaokaiqiang <914052561@qq.com>
2017-07-30 01:12:47 +0800

    Initial commit

---
+++ src/main/java/com/qkq/test/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.qkq.test;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+public class HelloWorld {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/qkq/test/Test.java
@@ -0,0 +1,10 @@
+package com.qkq.test;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+public class Test {
+    public static void main(String... args){
+        System.out.print("hello World");
+    }
+}
